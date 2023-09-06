6fc9b045cd3112653fb55725b3553a1036c633de
Vitamax <vitamaxoduo@gmail.com>
2023-09-06 03:29:48 +0300

    Done

--- lib/debug.py
+++ lib/debug.py
@@ -4,5 +4,6 @@ from config import CONN, CURSOR
 from song import Song
 
 
+
 if __name__ == '__main__':
     import ipdb; ipdb.set_trace()
--- lib/song.py
+++ lib/song.py
@@ -1,4 +1,28 @@
 from config import CONN, CURSOR
 
 class Song:
-    pass
+    def __init__(self, name, album) -> None:
+        self.name = name
+        self.album = album
+        
+        
+    @classmethod  
+    def create_table(cls):
+        sql = '''
+            CREATE TABLE IF NOT EXISTS songs (
+                id INT PRIMARY KEY,
+                name TEXT,
+                album TEXT
+            )
+        
+        '''
+        
+        CURSOR.execute(sql)
+        
+    
+    def save(self):
+        sql = '''
+        INSERT INTO songs (name, album)
+        VALUES(?, ?)
+        '''
+        CURSOR.execute(sql, (self.name, self.album))
